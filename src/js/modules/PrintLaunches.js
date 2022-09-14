df12225cf0357b093988d82491d3ef5adecec3cf
wilson639 <liam.wilson44@hotmail.com>
2022-09-14 18:06:52 +0100

    Add basic styles

--- src/js/modules/PrintLaunches.js
+++ src/js/modules/PrintLaunches.js
@@ -7,14 +7,16 @@ const printLaunches = (launches, rockets) => {
       if (rocket.id == launch.rocketId) rocketName = rocket.name;
     });
 
-    document.getElementById("launch-list").innerHTML +=
-    `<li class="launch">
-        <p>${launch.getConvertedDate()}</p>
-        <p>${launch.name}</p>
-        <p>${launch.success}</p>
-        <img src='${launch.getImage()}'></img>
-        <p>${launch.getDescription()}</p>
-        <p>${rocketName}</p> 
+    document.getElementById("launch-info").innerHTML +=
+    `<li class="launch-item col-sm-6">
+        <h2>${launch.name}</h2>
+        <div class="launch-image-container">
+          <span>${launch.getConvertedDate()}</span>
+          <img src='${launch.getImage()}' alt='${launch.name} launch'></img>
+        </div>
+        <p><span>Success: </span>${launch.success}</p>
+        <p><span>Description: </span>${launch.getDescription()}</p>
+        <p><span>Rocket: </span>${rocketName}</p> 
       </li>`
   })
 }
